5bd651a6600fa24eb719339081ab3b098de235ac
GitMurf <64155612+GitMurf@users.noreply.github.com>
2021-05-24 01:00:07 -0700

    v0.0.2 - First test release

--- main.ts
+++ main.ts
@@ -1,54 +1,59 @@
-import { Plugin, BlockCache } from "obsidian";
+import { Plugin, BlockCache, WorkspaceLeaf } from "obsidian";
+declare module "obsidian" {
+  interface View {
+    setCollapseAll(collapse: boolean): void;
+    setQuery(queryStr: string): void;
+  }
+}
 
 export default class BlockRefCounter extends Plugin {
   async onload() {
-    console.log("loading plugin");
+    console.log("loading plugin: Block Reference Counter");
     this.registerMarkdownPostProcessor((val, ctx) => {
-      const { blocks } = this.app.metadataCache.getCache(ctx.sourcePath);
+      const files = this.app.vault.getMarkdownFiles();
+      const blocks = this.app.metadataCache.getCache(ctx.sourcePath).blocks;
       if (blocks) {
-        const { lineStart } = ctx.getSectionInfo(val);
-        let parentBlocks: BlockCache[] = [];
-        for (const id in blocks) {
-          if (blocks[id].path === ctx.sourcePath) {
-            parentBlocks.push(blocks[id]);
-          }
-        }
-        parentBlocks.forEach((parentBlock) => {
-          let count = 0;
-          const files = this.app.vault.getMarkdownFiles();
-          files.forEach((file) => {
-            if (file.path !== ctx.sourcePath) {
-              const { embeds, links } = this.app.metadataCache.getFileCache(
-                file
-              );
+        const sectInfo = ctx.getSectionInfo(val);
+        const lnStart = sectInfo.lineStart;
+        const lnEnd = sectInfo.lineEnd;
+        Object.values(blocks).forEach((eachBlock) => {
+          const blockStart = eachBlock.position.start.line;
+          const blockEnd = eachBlock.position.end.line;
+          if (blockStart >= lnStart && blockEnd <= lnEnd) {
+            //console.log(this.app.metadataCache.getCache(ctx.sourcePath));
+            //console.log(ctx.getSectionInfo(val));
+            //console.log(blocks);
+            let count = 0;
+            files.forEach((eachFile) => {
+              const { embeds, links } = this.app.metadataCache.getFileCache(eachFile);
               if (embeds) {
-                embeds.forEach((embed) => {
-                  const id = embed.link.split("^")[1];
-                  if (id === parentBlock.id) {
-                    count++;
-                  }
+                const embedMatch = embeds.filter((embed) => {
+                  if (embed.link.split("^")[1] === eachBlock.id) { return true }
                 });
+                count = count + embedMatch.length;
               }
               if (links) {
-                links.forEach((link) => {
-                  const id = link.link.split("^")[1];
-                  if (id === parentBlock.id) {
-                    count++;
-                  }
+                const linkMatch = links.filter((link) => {
+                  if (link.link.split("^")[1] === eachBlock.id) { return true }
                 });
+                count = count + linkMatch.length;
               }
-            }
-          });
-          if (count > 0 && parentBlock.position.start.line === lineStart) {
-            const countEl = createEl("button", { cls: "count" });
-            countEl.innerText = count.toString();
-            countEl.on("click", "button", () => {
-              const search = this.app.workspace.getLeavesOfType("search")[0];
-              search.view.setCollapseAll(false);
-              search.view.setQuery(parentBlock.id + "]]");
-              this.app.workspace.revealLeaf(search);
             });
-            val.appendChild(countEl);
+            if (count > 0) {
+              const countEl = createEl("button", { cls: "count" });
+              countEl.innerText = count.toString();
+              countEl.on("click", "button", () => {
+                const search: WorkspaceLeaf = this.app.workspace.getLeavesOfType("search")[0];
+                search.view.setCollapseAll(false);
+                search.view.setQuery(eachBlock.id + "]]");
+                this.app.workspace.revealLeaf(search);
+              });
+              if (blockStart !== lnStart) {
+                const botPos = (lnEnd - blockEnd) * 40;
+                countEl.style.bottom = botPos + 'px';
+              }
+              val.appendChild(countEl);
+            }
           }
         });
       }
@@ -56,6 +61,6 @@ export default class BlockRefCounter extends Plugin {
   }
 
   onunload() {
-    console.log("unloading plugin");
+    console.log("unloading plugin: Block Reference Counter");
   }
 }
