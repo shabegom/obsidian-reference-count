a38f7282049fa9d8f58b924f6286fae3988b0692
Sam <sam@shbgm.ca>
2021-05-23 20:25:07 -0400

    multiple refs in one note

--- main.ts
+++ main.ts
@@ -7,40 +7,44 @@ export default class BlockRefCounter extends Plugin {
       const { blocks } = this.app.metadataCache.getCache(ctx.sourcePath);
       const sectionLine = ctx.getSectionInfo(val).lineStart;
       if (blocks) {
-        let parentBlock: BlockCache;
+        let parentBlocks: BlockCache[] = [];
         let count = 0;
         for (const id in blocks) {
           if (blocks[id].path === ctx.sourcePath) {
-            parentBlock = blocks[id];
+            parentBlocks.push(blocks[id]);
           }
         }
-        const files = this.app.vault.getMarkdownFiles();
-        files.forEach((file) => {
-          if (file.path !== ctx.sourcePath) {
-            const { embeds, links } = this.app.metadataCache.getFileCache(file);
-            if (embeds) {
-              embeds.forEach((embed) => {
-                const id = embed.link.split("^")[1];
-                if (id === parentBlock.id) {
-                  count++;
-                }
-              });
+        parentBlocks.forEach((parentBlock) => {
+          const files = this.app.vault.getMarkdownFiles();
+          files.forEach((file) => {
+            if (file.path !== ctx.sourcePath) {
+              const { embeds, links } = this.app.metadataCache.getFileCache(
+                file
+              );
+              if (embeds) {
+                embeds.forEach((embed) => {
+                  const id = embed.link.split("^")[1];
+                  if (id === parentBlock.id) {
+                    count++;
+                  }
+                });
+              }
+              if (links) {
+                links.forEach((link) => {
+                  const id = link.link.split("^")[1];
+                  if (id === parentBlock.id) {
+                    count++;
+                  }
+                });
+              }
             }
-            if (links) {
-              links.forEach((link) => {
-                const id = link.link.split("^")[1];
-                if (id === parentBlock.id) {
-                  count++;
-                }
-              });
-            }
+          });
+          if (count > 0 && parentBlock.position.start.line === sectionLine) {
+            const countEl = createEl("div", { cls: "count" });
+            countEl.innerText = count.toString();
+            val.appendChild(countEl);
           }
         });
-        if (count > 0 && parentBlock.position.start.line === sectionLine) {
-          const countEl = createEl("div", { cls: "count" });
-          countEl.innerText = count.toString();
-          val.appendChild(countEl);
-        }
       }
     });
   }
