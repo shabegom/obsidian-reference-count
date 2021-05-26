28ff100053dbc98e58610dcf2b8c6457ee78aabb
Sam <sam@shbgm.ca>
2021-05-26 10:56:04 -0400

    update all the times!

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ export default class BlockRefCounter extends Plugin {
     private cacheUpdate: EventRef;
     private layoutReady: EventRef;
     private layoutChange: EventRef;
+    private activeLeafChange: EventRef;
     async onload(): Promise<void> {
         console.log("loading plugin: Block Reference Counter")
 
@@ -20,9 +21,6 @@ export default class BlockRefCounter extends Plugin {
             const {blocks, embeds, links} = this.app.metadataCache.getFileCache(file)
             buildIndexObjects({ blocks, embeds, links, file })
             updateIndex()
-        })
-
-        this.layoutChange = this.app.workspace.on("layout-change", () => {
             const view = this.app.workspace.activeLeaf.view 
             const sourcePath = view.file.path
             const sections = view.previewMode.renderer.sections
@@ -34,14 +32,51 @@ export default class BlockRefCounter extends Plugin {
                 addBlockReferences({app: this.app, ctx: {getSectionInfo, sourcePath}, val })
             })
         })
+
+        this.layoutChange = this.app.workspace.on("layout-change", () => {
+            console.log("layout change")
+            const view = this.app.workspace.activeLeaf.view 
+            const sourcePath = view.file.path
+            const sections = view.previewMode.renderer.sections
+            sections.forEach(section => {
+                const lineStart = section.lineStart
+                const lineEnd = section.lineEnd
+                const val = section.el
+                const getSectionInfo = (val) => ({lineStart, lineEnd, text: ""})
+                addBlockReferences({app: this.app, ctx: {getSectionInfo, sourcePath}, val })
+            })
+        })
+
+        this.activeLeafChange = this.app.workspace.on("active-leaf-change", (leaf) => {
+            console.log("active leaf change")
+            const view = leaf.view
+            const sourcePath = view.file.path
+            const sections = view.previewMode.renderer.sections
+            sections.forEach(section => {
+                const lineStart = section.lineStart
+                const lineEnd = section.lineEnd
+                const val = section.el
+                const getSectionInfo = (val) => ({lineStart, lineEnd, text: ""})
+                addBlockReferences({app: this.app, ctx: {getSectionInfo, sourcePath}, val })
+            })
+        })
+
+        this.registerMarkdownPostProcessor((val, ctx) => {
+            addBlockReferences({app: this.app, ctx, val})
+        })
   
     }
+
+
+  
+
 
     onunload(): void {
         console.log("unloading plugin: Block Reference Counter")
         this.app.metadataCache.offref(this.cacheUpdate)
         this.app.workspace.offref(this.layoutReady)
         this.app.workspace.offref(this.layoutChange)
+        this.app.workspace.offref(this.activeLeafChange)
     }
 }
 
@@ -49,7 +84,6 @@ function addBlockReferences({ app, ctx, val }: AddBlockReferences): void {
     const { lineStart, lineEnd } = ctx.getSectionInfo(val) || {}
     //console.log(`markdownPostProcessor: Ln${lineStart}-${lineEnd}`)
     const { blocks, listItems, sections } = app.metadataCache.getCache(ctx.sourcePath) || {}
-
     if (blocks) {
         const matchedBlock = Object.values(blocks).find((eachBlock) => { if (eachBlock.position.start.line >= lineStart && eachBlock.position.start.line <= lineEnd) { return true } else { return false } })
         if (matchedBlock) {
@@ -69,7 +103,7 @@ function addBlockReferences({ app, ctx, val }: AddBlockReferences): void {
 
             Object.values(blocks).forEach((block) => {
                 const myId = `${thisFile.basename}^${block.id}`
-                if (blockRefs[myId] && blockRefs[myId].count > 0) {
+                if (blockRefs[myId] && blockRefs[myId]) {
                     if (sections) {
                         sections.forEach(section => {
                             if (section.id === block.id && lineStart === block.position.start.line) {
@@ -96,6 +130,7 @@ function addBlockReferences({ app, ctx, val }: AddBlockReferences): void {
 }
 
 function createButtonElement({app, blockRefs, val }: CreateButtonElement): void {
+    const existingButton = val.querySelectorAll(".count").item(0)
     const countEl = createEl("button", { cls: "count" })
     countEl.innerText = blockRefs.count.toString()
     const refTable: HTMLElement = createTable({app, val, files: Array.from(blockRefs.references)})
@@ -106,7 +141,11 @@ function createButtonElement({app, blockRefs, val }: CreateButtonElement): void 
             val.removeChild(refTable)
         }
     })
+    existingButton && existingButton.remove()
+    if (blockRefs.count > 0) {
     val.prepend(countEl)
+    }
+
 }
 
 function createTable({app, val, files}: {app: App, val: HTMLElement, files: FileRef[]}) {
